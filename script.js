f3ed105da1fef51b073d20590eb9255f6e4f7c93
Robert Anderson <reanderson89@gmail.com>
2020-09-28 21:39:58 -0400

    added files from main gt-pt, added a page to git hub.

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log("hello world");
+//  DOM variables
+
+// Javascript variables
+
+// function definitions
+
+//  function calls
+
+// event listeners
+
+
+});
