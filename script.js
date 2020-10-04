2ca1ccbd6a128296da52e21f6f85666009c3a18a
Robert Anderson <reanderson89@gmail.com>
2020-10-04 15:39:31 -0400

    A lot happened since my last commit... I added the moment.js so I can set date and time functions. I have not set them yet. I finished created a function that populates the page with all of the rows for the hours of the workday. I also set up the local storage so that the informtion you enter can be saved on click and will re-populate when you refresh the page.

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
 $(document).ready(function(){
-    console.log("hello world");
 //  DOM variables
 
+// var newRow = $("<tr>").addClass("d-flex");
 // Javascript variables
-
+var hourArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
 // function definitions
 
-//  function calls
+function createRows() {
+    
 
+    for (var i = 0; i < hourArray.length;i++) {
+
+        var savedText = localStorage.getItem(hourArray[i]);
+        if (savedText === null){
+            savedText="";
+        };
+        var newRow = $("<div>").addClass("row d-flex time-block past").attr("id", hourArray[i]);
+        
+        newRow.append($("<div>" + hourArray[i] + "</div>").addClass("col-1 hour"));
+
+        newRow.append($("<textarea placeholder='Add Event'>" + savedText + "</textarea>").addClass("col-10"));
+
+        newRow.append($("<button type='button submit' class='btn btn-primary saveBtn col-1'>Save</button>"));
+
+        $(".container").append(newRow);
+
+       
+        
+    };
+    $("button").on("click", function() {
+        var userInput = $(this).siblings("textarea").val();
+        var hour = $(this).siblings("div").text();
+        console.log(userInput);
+        console.log(hour);
+        localStorage.setItem(hour, userInput);
+    });
+     
+
+};
+
+//    function saveEvent() {
+//        $(".btn").on("click", function(event){
+
+//            var addEvent = JSON.parse(window.localStorage.getItem("userInput"));
+//            window.localStorage.setItem("userInput", JSON.stringify(addEvent));
+//            console.log(this);
+//            console.log("User Input: " + addEvent);
+           
+//        }
+//        );
+//    }
+
+
+
+// function changeColor() {
+//     if (hourArray[5] < 12){
+//         $(".time-block").addClass("past");
+//     }
+// }
+
+//  function calls
+createRows();
+// saveEvent();
+// changeColor();
 // event listeners
 
 
