a145ecd36b1bcb5b7aca30b2dbd6f6f00ec815d2
Robert Anderson <reanderson89@gmail.com>
2020-10-05 15:30:20 -0400

    added a confirm style page alert to ask user if they are sure they want to clear all events after clicking the button. Just as a precaution.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# work-day-scheduler
+# **Work-Day-Scheduler**
+
+## **Live Link**
+ https://reanderson89.github.io/work-day-scheduler/
+
+## **Contributors**
+@reanderson89
+
+## **Description**
+This application allows the user to enter and save information into each given hour of their work day (9AM-5PM). The user can click on the input field and enter in their events and then click the save button icon to store their information locally on their browser. They can close the page or refresh it and the information will be readily available to them. If they would like to clear all of the events, there is a button at the bottom that will clear saved information from this application from their local storage. It will also update the page so that it shows all fields to be empty once again.
+
+The time blocks are all color coded to show past(grey), present(red), and future(green) hours. This will help the user visually with being able to see where they are at in their day.
+
+## **Technology Stack**
+
+* HTML, CSS, Javascript, Jquery, Bootstrap
+
+## **Demo/Images**
+![demo gif](assets/gifs/WorkDayGif.gif)
+
+## **Contact**
+
+* #### **Name:** Robert Anderson (@reanderson89)
+* #### **Email:** [reanderson89@gmail.com](reanderson89@gmail.com)
+
+## **License**
+MIT
--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
-//  DOM variables
 
-// Javascript variables
 var hourArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
 var militaryHoursArray = ["9","10","11","12","13","14","15","16","17"];
-// function definitions
 
-
+// Displays and updates the current time live
 var currentTime = setInterval( function(){
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     document.getElementById("currentDay").innerHTML = time;  
 });
 
-
+// Changes the clss of the time blocks depending on if they have past, are current, or are still to come.
 function updateBlocks(){
 var currentHour = moment().hours();
 $(".time-block").each(function(){
@@ -31,10 +28,14 @@ $(".time-block").each(function(){
 })
 };
 
+// Dynamically creates new rows for each time block and adds necessary information to it
 function createRows() {
     for (var i = 0; i < hourArray.length;i++) {
 
+        // gets the users input from local storage
         var savedText = localStorage.getItem(hourArray[i]);
+
+        // ensures that a block without any saved information will show it's placeholder instead of saying "null"
         if (savedText === null){
             savedText="";
         };
@@ -49,6 +50,8 @@ function createRows() {
         $(".container").append(newRow);
   
     };
+
+    // saves the users input into local storage
     $(".saveBtn").on("click", function() {
         var userInput = $(this).siblings("textarea").val();
         var hour = $(this).siblings("div").text();
@@ -57,20 +60,25 @@ function createRows() {
         localStorage.setItem(hour, userInput);
     });
      
+    // clears all saved events from local storage and clears all of the input fields
     $("#clear-events").on("click", function(event){
         event.preventDefault();
-        window.localStorage.clear();
+        // window.localStorage.clear();
+        // $(".time-block").remove();
+        // createRows();
+        var clearAll = confirm("Are you sure you would like to clear ALL of the events?");
+        if (clearAll === true){
+            window.localStorage.clear();
         $(".time-block").remove();
         createRows();
-    })
+        } else {
+            return;
+        };
+    });
 updateBlocks();
 };
 
-
-
-//  function calls
+// This will run when the page is opened
 createRows();
-// event listeners
-
 
 });
